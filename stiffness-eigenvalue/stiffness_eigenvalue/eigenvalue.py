690fd93293c684ffe1a82dd1a45d7534f5f7187d
KentaroKajiyama <kemushi555@icloud.com>
2024-08-01 05:50:05 +0900

    add: 最小固有値計算の雛形
    テストしたら理論値とかなりずれたので修正が必要。

--- stiffness-eigenvalue/stiffness_eigenvalue/eigenvalue.py
+++ stiffness-eigenvalue/stiffness_eigenvalue/eigenvalue.py
@@ -1,5 +1,16 @@
 import numpy as np
 import math
+from scipy.optimize import minimize
+from functools import partial
+
+"""
+EPS_OBJ : A small constant for objective function 
+BIG_C : A big constant for optimization
+"""
+###########################
+EPS_OBJ = 10**(-8)
+BIG_C = 10**6
+###########################
 
 # 回転行列（歪対称行列）の生成
 def gen_skew_symmetric(d):
@@ -13,14 +24,12 @@ def gen_skew_symmetric(d):
   for i in range(0,d):
     result.append(np.eye(d))
   return result
-
 # 平行移動ベクトルの生成
 def gen_parallel_vector(d):
   # 回転用の余分なt（ただの0ベクトル）
   result = [np.zeros(d) for _ in range(0,math.comb(d,2))]
   [result.append(np.array([0 if i!=j else 1 for i in range(0,d)])) for j in range(0,d)]
   return result
-
 # 基底の生成
 def gen_basis(d,p):
   # 点の個数
@@ -35,4 +44,20 @@ def gen_basis(d,p):
       x.append(x_j)
     basis_box.append(np.array(x))
   return basis_box
-
+# 目的関数
+def objective(x,L):
+  if np.linalg.norm(x) <= EPS_OBJ:
+    return 0
+  else:
+    return np.dot(x.T, np.dot(L, x))/np.dot(x.T, x)
+# 固有値計算（最適化による計算）。固有値と固有ベクトルを返す。
+def min_non_zero_eigen(L, x0, d, p):
+  # 近似行列の生成
+  basis_box = gen_basis(d,p)
+  L_tilde = L
+  for basis in basis_box:
+    L_tilde += BIG_C*np.outer(basis, basis)
+  # 目的関数
+  obj_func = partial(objective, L=L_tilde)
+  opt = minimize(obj_func, x0)
+  return opt.fun, opt.x
--- stiffness-eigenvalue/tests/test_eigenvalue.py
+++ stiffness-eigenvalue/tests/test_eigenvalue.py
@@ -1,5 +1,8 @@
 import numpy as np
-from stiffness_eigenvalue.eigenvalue import gen_skew_symmetric, gen_parallel_vector, gen_basis
+import networkx as nx
+import math
+from stiffness_eigenvalue.eigenvalue import gen_skew_symmetric, gen_parallel_vector, gen_basis, min_non_zero_eigen
+from stiffness_eigenvalue.framework import stiffness_matrix
 
 # Sとtの生成のテスト
 def test1():
@@ -18,7 +21,36 @@ def test2():
   basis_box = gen_basis(dim,p)
   for i, basis in enumerate(basis_box):
     print(f"{i}th basis: {basis}")
+# 最小固有値のテスト
+def test3():
+  # 各定数
+  d = 2
+  V = 6
+  D = math.comb(d+1,2)
+  # 完全グラフの生成
+  G_comp = nx.complete_graph(V)
+  # 辺集合
+  bonds = np.array(list(G_comp.edges()))
+  # position of sites
+  p = 5*np.random.randn(d*V).reshape(-1,d)
+  # 初期固有ベクトル
+  x0 = 3*np.random.randn(d*V)
+  # stiffness matrix
+  L = stiffness_matrix(p, bonds)
+  # ライブラリーと手動実装の比較
+  # ライブラリー
+  lib_eigvals, lib_eigvecs = np.linalg.eig(L)
+  sorted_indices = np.argsort(lib_eigvals)
+  lib_eigvals_sorted, lib_eigvecs_sorted = lib_eigvals[sorted_indices], lib_eigvecs[sorted_indices]
+  lib_eigval, lib_eigvec = lib_eigvals_sorted[D], lib_eigvecs_sorted[D] # 0 baseなのでD+1番目だがインデックスはDであることに注意
+  # 手動実装
+  hand_eigval, hand_eigvec = min_non_zero_eigen(L, x0, d, p)
+  print("library vs hand")
+  print("Eigenvalue")
+  print(f"lib:{lib_eigval}, hand:{hand_eigval}")
+  print("Eigenvector")
+  print(f"lib:{lib_eigvec}, hand:{hand_eigvec}")
   
 
 if __name__ == "__main__":
-  test2()
+  test3()
