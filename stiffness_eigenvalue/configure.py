3d6d6f92f90378bdcee4282e344785d4671485b5
KentaroKajiyama <kemushi555@icloud.com>
2025-01-24 17:53:59 +0900

    fix

--- stiffness_eigenvalue/configure.py
+++ stiffness_eigenvalue/configure.py
@@ -2,6 +2,6 @@ import sys
 from dotenv import load_dotenv
 import os
 
-load_dotenv("~/experiment0124/stiffness-eigenvalue/config/.env")
+load_dotenv("config/.env")
 print(sys.path)
 MAX_ITER_FOR_ARMIJO = int(os.getenv("MAX_ITER_FOR_ARMIJO"))
